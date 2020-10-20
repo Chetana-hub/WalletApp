6015a8bfdcb4912571021949c5e168b3101d2a81
vidyasreem <73115140+vidyasreem@users.noreply.github.com>
2020-10-20 14:58:03 +0530

    check2
    getters and setters of benificiary details

--- src/main/java/com/cg/paymentapp/beans/BenificiaryDetails.java
+++ src/main/java/com/cg/paymentapp/beans/BenificiaryDetails.java
@@ -5,6 +5,29 @@ public class BenificiaryDetails {
 	private int benificiaryId;
 	private String name;
 	private String mobileNumber;
+	public int getBenificiaryId() {
+		return benificiaryId;
+	}
+	public void setBenificiaryId(int benificiaryId) {
+		this.benificiaryId = benificiaryId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	@Override
+	public String toString() {
+		return "BenificiaryDetails [benificiaryId=" + benificiaryId + ", name=" + name + ", mobileNumber="
+				+ mobileNumber + "]";
+	}
 
 }
 
