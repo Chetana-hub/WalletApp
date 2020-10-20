7d5197c73ff7a695c9d6af6c0f8fe9b6690a1f30
jyotibatham1997 <jyotibatham1997@gmail.com>
2020-10-20 14:54:09 +0530

    check1
    getter setters

--- src/main/java/com/cg/paymentapp/beans/BankAccount.java
+++ src/main/java/com/cg/paymentapp/beans/BankAccount.java
@@ -7,5 +7,40 @@ public class BankAccount {
 	private String bankName;
 	private double balance;
 	private Wallet wallet;
+	public int getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(int accountNo) {
+		this.accountNo = accountNo;
+	}
+	public String getIfscCode() {
+		return ifscCode;
+	}
+	public void setIfscCode(String ifscCode) {
+		this.ifscCode = ifscCode;
+	}
+	public String getBankName() {
+		return bankName;
+	}
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public Wallet getWallet() {
+		return wallet;
+	}
+	public void setWallet(Wallet wallet) {
+		this.wallet = wallet;
+	}
+	@Override
+	public String toString() {
+		return "BankAccount [accountNo=" + accountNo + ", ifscCode=" + ifscCode + ", bankName=" + bankName
+				+ ", balance=" + balance + ", wallet=" + wallet + "]";
+	}
 }
 
