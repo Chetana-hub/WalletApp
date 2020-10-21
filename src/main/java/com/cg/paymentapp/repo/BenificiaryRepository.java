60493c294543b55e7253d3732f6c2eeb90e945bd
sowmyabit <sowmyaballari69@gmail.com>
2020-10-21 21:02:18 +0530

    sowmyacheck1

---
+++ src/main/java/com/cg/paymentapp/repo/BenificiaryRepository.java
@@ -0,0 +1,38 @@
+package com.cg.paymentapp.repo;
+
+import com.cg.paymentapp.beans.BenificiaryDetails;
+import com.cg.paymentapp.beans.Customer;
+
+public class BenificiaryRepository implements IBenificiaryRepository {
+
+	@Override
+	public BenificiaryDetails addBenificiary(BenificiaryDetails bd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BenificiaryDetails updateBenificiary(BenificiaryDetails bd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BenificiaryDetails deleteBenificiary(BenificiaryDetails bd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BenificiaryDetails viewBenificiary(BenificiaryDetails bd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BenificiaryDetails viewAllBenificiary(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
